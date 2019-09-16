b86706b83ba788592bc6bd9d6cd0a5ac0fe43388
tcarrier2 <53099878+tcarrier2@users.noreply.github.com>
2019-09-16 17:46:50 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ var fs = require('fs');
 var url = require('url');
 var canv = require('canvas');
 var querystring = require('querystring');
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database(':memory:', (err) => {
+  if(err){
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+db.serialize(() => {
+  db.each(`CREATE TABLE all_image_filenames (image_filename VARCHAR(255))`, (err, row) => {
+    if(err){
+      console.error(err.message);
+    }
+  });
+});
 
 http.createServer(function (req, res){
   req.setEncoding('binary'); 
@@ -23,8 +39,21 @@ http.createServer(function (req, res){
         var shorter_data = entire_data.substring(upper_boundary); 
         var binary_data = shorter_data.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 
-        fs.writeFile("temporary.png", binary_data, 'binary', function(err){
+        var image_file_info = note['Content-Disposition'].split('; ');
+        var image_filename = image_file_info[2].substring(10, image_file_info[2].length-1);
+
+        fs.writeFile("archives//" + image_filename, binary_data, 'binary', function(err){
           res.end();
+        });
+
+        sql_insert = `INSERT INTO all_image_filenames (image_filename) VALUES ('` + image_filename + `')`;
+
+        db.serialize(() => {
+          db.each(sql_insert, (err, row) => {
+            if(err){
+              console.error(err.message);
+            }
+          });
         });
       } 
     });
@@ -37,6 +66,40 @@ http.createServer(function (req, res){
     if(filename == "./"){
       fs.readFile('index.html', function(err, data){
         res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
+    }
+    else if(filename == "./results"){
+      fs.readFile('results.html', function(err, data){  
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
+    }
+    else if(filename == "./all_image_filenames.json"){
+      var all_image_filenames_object = {}
+      all_image_filenames_object.array = [];
+
+      db.all(`SELECT image_filename FROM all_image_filenames`, (err, rows) => {
+        rows.forEach(function (row){
+          all_image_filenames_object.array.push(row.image_filename);
+        })
+        all_image_filenames_string = JSON.stringify(all_image_filenames_object);
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(all_image_filenames_string);
+        res.end();
+        if(err){
+          console.error(err.message);
+        }
+      });
+    }
+    else if(filename.includes("jpeg") || filename.includes("jpg") || filename.includes("png")){
+      var image_filename;
+      image_filename = "archives" + q.pathname;
+
+      fs.readFile(image_filename, function(err, data){  
+        res.writeHead(200, {'Content-Type': 'image/jpeg'});
         res.write(data);
         res.end();
       });
